219f95e73516b6a8cbc4ceb8887b6a5789f82a49
zakaria-ahmad <deoterssekati@gmail.com>
2024-05-15 19:03:29 +0700

    first commit

---
+++ penambahan.js
@@ -0,0 +1,7 @@
+var a = 10, b = 10;
+
+var a = ++a;
+var b = --b;
+
+console.log(a);
+console.log(b);
---
+++ switch.js
@@ -0,0 +1,16 @@
+let hari = 3
+switch (hari) {
+    case 1:
+    pesan = "hari minggu"
+    break;
+
+    case 2:
+    pesan = "hari sabtu"
+    break;
+
+    default:
+    pesan = "hari kerja"
+    break;
+
+    console.log(hari);
+}
